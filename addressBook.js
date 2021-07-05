64f9f472bc5b3e1c5e4bd977ef536fe0d4cc20eb
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-07-05 20:40:10 +0530

    REFACTOR:To user input operation in address book problem

--- addressBook.js
+++ addressBook.js
@@ -95,8 +95,7 @@ class Contact{
 
 
 
-//Failure cases:
-{
+
     //incorrect first name:
     try{
         let contact2 = new Contact("aAAA","Lad","krantinagar,kundal","Sangli","MH","416309","8888310299","ladnilesh@gemail.com");
@@ -136,67 +135,67 @@ class Contact{
 
 
    
-}
+
 
 //new address book 
 let addressBookArray = new Array();
 //pass by object value
 let contact = new Contact("Nilesh","Lad","krantinagar,kundal","Sangli","MH","416309","8888310299","ladnilesh@gemail.com");
-console.log(contact.toString());
+// console.log(contact.toString());
+
 addContact(new Contact("Mark","Zuckerberg","Street 191","NewYork","New York","732106","11 6182755450","mark@email.com"));
 addContact(new Contact("Bill","Gates","Street 250","Medina","Washington","723091","11 6817263541","bill@email.com"));
 addContact(new Contact("Jeff","Bezos","Street 200","CityABC","Washington","772109","11 6385755850","jeff@email.com"));
 addContact(new Contact("Mark","Gates","Street 199","NewYork","New York","722106","11 6182755777","markgates@email.com"));
 addContact(new Contact("Bill","Bezos","Street 218","NewYork","New York","992106","11 6182755000","markgates@email.com"));
-console.log(addressBookArray.toString());
+ console.log(addressBookArray.toString());
 
 
 //UC4 USING ARROW FUNCTION 
-{
-    let contactToEdit = addressBookArray.find(contact=>contact.firstName=="Jeff"&&contact.lastName=="Bezos");
-    if(contactToEdit!=undefined){
-        contactToEdit.phone = "11 6123456789";
-        console.log("\nUsing arrow => function "+addressBookArray);
-    }
-    else 
-        console.log("\nContact not found");
-}
+
+    // let contactToEdit = addressBookArray.find(contact=>contact.firstName=="Jeff"&&contact.lastName=="Bezos");
+    // if(contactToEdit!=undefined){
+    //     contactToEdit.phone = "11 6123456789";
+    //     console.log("\n Using arrow => function "+addressBookArray);
+    // }
+    // else 
+    //     console.log("\n Contact not found");
+
 
 //UC4 USING SEPARATE FUNCTION
-{
+
     function editContact(contact){
         if (contact.firstName == "Jeff" && contact.lastName == "Bezos"){
             contact.phone = "11 6123456788";
             return contact;
         }
     }
-    let contactToEdit = addressBookArray.find(editContact);
-    console.log("\nUsing separate function: "+addressBookArray);
-} 
+    // contactToEdit = addressBookArray.find(editContact);
+    // console.log("\nUsing separate function: "+addressBookArray);
+
 
 
 //UC5 DELETE A CONTACT
-{
+
     for(let i=0;i<addressBookArray.length;i++){
         if(addressBookArray[i].firstName=="Jeff"
             &&addressBookArray[i].lastName=="Bezos")
             delete addressBookArray[i];
     }
-    console.log("\nAddress Book Array After Deleting Contact: "+addressBookArray)
-}
+    // console.log("\nAddress Book Array After Deleting Contact: "+addressBookArray)
 
-//UC6 NUMBER OF CONTACTS IN ADDRESS BOOK USING REDUCE IN SEPARATE FUNCTION
-{
+
+//UC6 NUMBER OF CONTACTS IN ADDRESS BOOK USING REDUCE IN SEPARATE FUNCTION{
     function totalContacts(totalContacts){
         return ++totalContacts;
     }
     console.log("\nTotal Contacts With reduce using separate function: "+addressBookArray.reduce(totalContacts,0)); 
-}
+
 
 //UC6 NUMBER OF CONTACTS IN ADDRESS BOOK USING ARROW FUNCTION TO REDUCE
-{
-    console.log("Total Contacts With reduce using arrow => function: "+addressBookArray.reduce((totalContacts)=>++totalContacts,0));
-}
+
+    // console.log("Total Contacts With reduce using arrow => function: "+addressBookArray.reduce((totalContacts)=>++totalContacts,0));
+
 
 
 //UC7 Add contact if not already present:
@@ -211,19 +210,20 @@ function addContact(newContact){
 }
 
 //Trying to add duplicate contact:
-addContact(new Contact("Mark","Zuckerberg","Street 191","NewYork","New York","732106","11 6182755450","mark@email.com"));
-console.log(addressBookArray.toString());
+// addContact(new Contact("Mark","Zuckerberg","Street 191","NewYork","New York","732106","11 6182755450","mark@email.com"));
+// console.log(addressBookArray.toString());
 
 
 //UC 8 Search Person In a City Or State
 function searchPersonInCity(firstName,city){
     return addressBookArray.find(contact=>contact.firstName==firstName&&contact.city==city);
 }
+
 function searchPersonInState(firstName,state){
     return addressBookArray.find(contact=>contact.firstName==firstName&&contact.state==state);
 }
-console.log("Search Person in a city : "+searchPersonInCity("Mark","NewYork"));
-console.log("Search Person in a state : "+searchPersonInState("Mark","New York")); 
+// console.log("Search Person in a city : "+searchPersonInCity("Mark","NewYork"));
+// console.log("Search Person in a state : "+searchPersonInState("Mark","New York")); 
 
 
 //UC9 View Persons By city or state
@@ -236,8 +236,8 @@ function viewPersonsByCityOrState(name,type){
     else 
         console.log("Invalid choice");
 }
-viewPersonsByCityOrState("NewYork","City");
-viewPersonsByCityOrState("New York","State");
+// viewPersonsByCityOrState("NewYork","City");
+// viewPersonsByCityOrState("New York","State");
 
 
 //UC10 Count persons by city or state
@@ -251,8 +251,8 @@ function countPersonsByCityOrState(name,type){
         console.log("Invalid choice");
     return count;
 }
-console.log("\n Number of contacts in NewYork City: "+countPersonsByCityOrState("NewYork","City"));
-console.log("Number of contacts in New York State: "+countPersonsByCityOrState("New York","State"));
+// console.log("\n Number of contacts in NewYork City: "+countPersonsByCityOrState("NewYork","City"));
+// console.log("Number of contacts in New York State: "+countPersonsByCityOrState("New York","State"));
 
 
 //UC 11 Sort Entries alphabetically
@@ -267,13 +267,66 @@ function sortAddressBookBy(type){
     case "zip":
         return addressBookArray.slice().sort((a,b)=>(a.zip).localeCompare(b.zip));
     }
-
-
     
 }
 
-let sortedByName = sortAddressBookBy("name");
-    console.log("\nSorted address book by name:" +sortedByName.toString());
-    console.log("Sorted address book by city: "+sortAddressBookBy("city"));
-    console.log("Sorted address book by state: "+sortAddressBookBy("state"));
-    console.log("Sorted address book by zip: "+sortAddressBookBy("zip"));
+// let sortedByName = sortAddressBookBy("name");
+//     console.log("\nSorted address book by name:" +sortedByName.toString());
+//     console.log("Sorted address book by city: "+sortAddressBookBy("city"));
+//     console.log("Sorted address book by state: "+sortAddressBookBy("state"));
+//     console.log("Sorted address book by zip: "+sortAddressBookBy("zip"));
+
+
+    // To compute a number taken as input
+
+    const prompt = require('prompt-sync')();
+    console.log("1.To address book data");
+    console.log("2.edite contact");
+    console.log("3.new address book");
+    console.log("4.serach person city and state");
+    console.log("5.view Persons By City Or State");
+    console.log("6.count Persons By City Or State");
+    console.log("7.sort Address Book By person name, state, city");
+    console.log("8.delete person");
+
+    let number = Number(prompt("Enter the choices : "));
+
+switch(number){
+    case 1:
+        console.log(addressBookArray.toString());
+        break;
+    case 2:
+        contactToEdit = addressBookArray.find(editContact);
+        console.log("\n Using separate function: "+addressBookArray);
+        break;
+    case 3:
+        addContact(new Contact("Mark","Zuckerberg","Street 191","NewYork","New York","732106","11 6182755450","mark@email.com"));
+        console.log(addressBookArray.toString());
+        break;
+    case 4:
+        console.log("Search Person in a city : "+searchPersonInCity("Mark","NewYork"));
+        console.log("Search Person in a state : "+searchPersonInState("Mark","New York")); 
+        break;
+    case 5:
+        viewPersonsByCityOrState("NewYork","City");
+        viewPersonsByCityOrState("New York","State");
+        break;
+    case 6:
+        console.log("\n Number of contacts in NewYork City: "+countPersonsByCityOrState("NewYork","City"));
+        console.log("Number of contacts in New York State: "+countPersonsByCityOrState("New York","State"));
+        break;
+    case 7:
+        let sortedByName = sortAddressBookBy("name");
+        console.log("\nSorted address book by name:" +sortedByName.toString());
+        // console.log("Sorted address book by city: "+sortAddressBookBy("city"));
+        // console.log("Sorted address book by state: "+sortAddressBookBy("state"));
+        // console.log("Sorted address book by zip: "+sortAddressBookBy("zip"));
+        break;   
+        case 8:
+            console.log("delete the jeff person ")
+        console.log("\nAddress Book Array After Deleting Contact: "+addressBookArray)
+        break;       
+ 
+    default:
+        console.log("Invalid");
+    }
---
+++ new.js
@@ -0,0 +1,15 @@
+// class Book extends Contact{
+//     //new address book 
+// let addressBookArray = new Array();
+// //pass by object value
+// // Contact contact = new Contact("Nilesh","Lad","krantinagar,kundal","Sangli","MH","416309","8888310299","ladnilesh@gemail.com");
+// // console.log(contact.toString());
+// Contact Book = new Contact("Nilesh","Lad","krantinagar,kundal","Sangli","MH","416309","8888310299","ladnilesh@gemail.com");
+
+// addContact(new Contact("Mark","Zuckerberg","Street 191","NewYork","New York","732106","11 6182755450","mark@email.com"));
+// addContact(new Contact("Bill","Gates","Street 250","Medina","Washington","723091","11 6817263541","bill@email.com"));
+// addContact(new Contact("Jeff","Bezos","Street 200","CityABC","Washington","772109","11 6385755850","jeff@email.com"));
+// addContact(new Contact("Mark","Gates","Street 199","NewYork","New York","722106","11 6182755777","markgates@email.com"));
+// addContact(new Contact("Bill","Bezos","Street 218","NewYork","New York","992106","11 6182755000","markgates@email.com"));
+// // console.log(addressBookArray.toString());
+// }
